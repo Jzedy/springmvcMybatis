f79f333b0e258826b36bcda94b3978eb0cf9ba0f
jzedy <jzedy@foxmail.com>
2018-02-24 18:05:09 +0800

    some extract code

--- src/main/java/com/cn/jzedy/controller/UsersController.java
+++ src/main/java/com/cn/jzedy/controller/UsersController.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.annotation.Resource;
-import java.util.List;
 
 @Controller
 public class UsersController {
@@ -17,10 +16,6 @@ public class UsersController {
 
     @RequestMapping(value = "/findAll")
     public String findAll(){
-//        List<Users> users = userService.findAll();
-//        for (Users user : users) {
-//            System.out.println(user.getUsername());
-//        }
         Users users = userService.findById("1");
         System.out.println(JSONObject.toJSONString(users));
         return "index";
--- src/main/webapp/WEB-INF/views/index.jsp
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -1,10 +1,3 @@
-<%--
-  Created by IntelliJ IDEA.
-  User: zhangyin
-  Date: 2018/2/24
-  Time: 17:25
-  To change this template use File | Settings | File Templates.
---%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
