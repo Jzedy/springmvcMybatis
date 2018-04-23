18343248cfa22cdc7235dc8641e0099d9fa27cd7
Jzedy <jzedy@foxmail.com>
2018-04-23 22:23:59 +0800

    使用@controllerAdvice添加统一异常处理

--- src/main/java/com/cn/jzedy/base/controller/BaseController.java
+++ src/main/java/com/cn/jzedy/base/controller/BaseController.java
@@ -1,14 +1,21 @@
 package com.cn.jzedy.base.controller;
 
+import com.cn.jzedy.base.response.ResponseContents;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by Jzedy on 2018/2/27
  */
 
+@ControllerAdvice
 public class BaseController {
 
     protected static final String REDIECT = "redirect:";
@@ -17,6 +24,20 @@ public class BaseController {
     @ModelAttribute
     public void getPath(HttpServletRequest request, Model model){
         model.addAttribute( "ctx", request.getContextPath() );
+    }
+
+    /**
+     * 统一异常处理
+     * @param e
+     * @return
+     */
+    @ExceptionHandler(value = Exception.class)
+    @ResponseBody
+    public Map errorHandler(Exception e){
+        Map map = new HashMap();
+        map.put("code", ResponseContents.KEY_ERROR);
+        map.put("msg",e.getMessage());
+        return map;
     }
 
 }
